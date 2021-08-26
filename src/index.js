4fc9ea560043bb0cdf90314695c10592752646ca
MaiRizk <mai.riizk@gmail.com>
2021-08-26 03:23:49 +0200

    Fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import './style.css';
 
 const tasks = [
-    {
-        description: "Complete first task",
-        completed: false,
-        index: 0
-    },
-    {
-        description: "Complete second task",
-        completed: false,
-        index: 1
-    },
+  {
+    description: 'Complete first task',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Complete second task',
+    completed: false,
+    index: 1,
+  },
 ];
 
 const ul = document.getElementById('list');
 
-tasks.forEach(function (task) {
-    var li = document. createElement("li");
-    li.appendChild(document. createTextNode(task.description));
-    ul. appendChild(li);
-    
+tasks.forEach((task) => {
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(task.description));
+  ul.appendChild(li);
 });
-
-
