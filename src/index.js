36b6ddf340fb3201a8aa110e4981b19224338168
MaiRizk <mai.riizk@gmail.com>
2021-08-26 03:12:54 +0200

    Complete first milestone requirments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.css';
+
+const tasks = [
+    {
+        description: "Complete first task",
+        completed: false,
+        index: 0
+    },
+    {
+        description: "Complete second task",
+        completed: false,
+        index: 1
+    },
+];
+
+const ul = document.getElementById('list');
+
+tasks.forEach(function (task) {
+    var li = document. createElement("li");
+    li.appendChild(document. createTextNode(task.description));
+    ul. appendChild(li);
+    
+});
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: '/',
   },
   module: {
     rules: [
