a79a44f0ca2494f05c5d75cec2ac5936b4f1e368
MaiRizk <mai.riizk@gmail.com>
2021-08-27 08:30:42 +0200

    Fix localStorage issue and object index issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './style.css';
 import onCheck from './status';
 
-const tasks = [
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [
   {
     description: 'Complete first task',
     completed: false,
@@ -14,19 +14,19 @@ const tasks = [
   },
 ];
 
+window.localStorage.setItem('tasks', JSON.stringify(tasks));
+
 const ul = document.getElementById('list');
 
 tasks.forEach((task) => {
   const li = document.createElement('li');
   const checkbox = document.createElement('input');
   checkbox.type = 'checkbox';
-  checkbox.value = (task.completed);
+  checkbox.checked = task.completed;
   li.appendChild(checkbox);
   li.appendChild(document.createTextNode(task.description));
   ul.appendChild(li);
   checkbox.addEventListener('change', () => {
-    onCheck(task);
+    onCheck(task.index);
   });
 });
-
-window.localStorage.setItem('task', JSON.stringify(tasks));
--- src/status.js
+++ src/status.js
@@ -1,5 +1,9 @@
-const onCheck = (task) => {
-  task.completed = !task.completed;
+const onCheck = (index) => {
+  const tasks = JSON.parse(localStorage.getItem('tasks'));
+  if (tasks.length) {
+    tasks[index].completed = !tasks[index].completed;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
 };
 
-export default { onCheck };
+export default onCheck;
