8c7bd11965753463d2a46d855f1cd1530cd25370
Leonardoeng13 <leonardoandrade.eng@gmail.com>
2021-09-03 18:36:31 -0300

    New files and functions

--- src/add-detele.test.js
+++ src/add-detele.test.js
@@ -2,24 +2,29 @@
  * @jest-environment jsdom
  */
 
-const addTask = require('./add-delete');
+// const addTask = require('./add-delete');
+
+import { mockHtml } from './mockhtml.js';
+import { addTask } from './function.js';
+/* const addTask = require('./function');
 const saveDataLocalStorage = require('./local-storage');
 const loadDataLocalStorage = require('./local-storage');
 const deleteTask = require('./add-delete');
 const editTask = require('./add-delete');
 const clearCompleted = require('./add-delete');
+const mockHtml = require('./mockhtml'); */
 
 describe('add and remove Task', () => {
   it('Add Task', () => {
-    let array = [];
-    array = loadDataLocalStorage();
-    array = addTask(1, 'test1');
-    saveDataLocalStorage(array);
-    expect(array).toHaveLength(1);
+    document.body.innerHTML = mockHtml;
+    addTask(1, 'test1');
+    // saveDataLocalStorage(array);
+    const list = document.querySelectorAll('#list li');
+    expect(list).toHaveLength(1);
   });
 
-  it('Update Task', () => {
-    let array = [];
+/*   it('Update Task', () => {
+    let array = ['test changed'];
     array = loadDataLocalStorage();
     array = editTask(1, 'test changed');
     saveDataLocalStorage(array);
@@ -40,5 +45,5 @@ describe('add and remove Task', () => {
     array = clearCompleted(1);
     saveDataLocalStorage(array);
     expect(array).toHaveLength(1);
-  });
+  }); */
 });
